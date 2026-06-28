7757fdac2519baa79c4a367ec16bed199c15afd1
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-28 13:58:52 +0530

    Add files via upload

---
+++ chapter2.py
@@ -0,0 +1,29 @@
+# Integers 
+black_tea_grams = 14
+ginger_grams = 5    
+
+total_grams = black_tea_grams + ginger_grams
+print(f"Total grams of tea: {total_grams}g  ")
+
+remaining_grams = black_tea_grams - ginger_grams
+print(f"Remaining grams of black tea after using ginger: {remaining_grams}g  ")
+
+mil_lites = 8 
+serving = 3 
+milk_per_serving = mil_lites / serving
+print(f"Milk per serving: {milk_per_serving} ")
+
+total_tea_bags = 22
+pots = 4
+tea_bags_per_pot = total_tea_bags // pots
+print(f"Tea bags per pot: {tea_bags_per_pot} ")
+
+total_cardamom_grams = 10
+pots = 4
+cardamom_per_pot = total_cardamom_grams % pots
+print(f"Cardamom grams left after making tea: {cardamom_per_pot}g  ")   
+
+base_flavor_strength = 5    
+scaling_factor = 2
+scaled_flavor_strength = base_flavor_strength ** scaling_factor
+print(f"Scaled flavor strength: {scaled_flavor_strength}  ")
