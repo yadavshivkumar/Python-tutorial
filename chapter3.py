15cef08a4319d2440a447e3bdf58b967bdc8de2a
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-28 13:59:35 +0530

    Add files via upload

---
+++ chapter3.py
@@ -0,0 +1,15 @@
+is_boiling = False
+string_count =  5
+total_actions= string_count+ is_boiling 
+print(f"Total actions: {total_actions}  ")
+
+
+mlk_present = 0 
+print(f"Is milk present? {bool(mlk_present)}  ")
+
+water_hot = True
+tea_added = False
+
+can_server = water_hot and tea_added
+print(f"Can serve tea? {can_server}  ") 
+
