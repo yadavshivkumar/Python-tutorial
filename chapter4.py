11d3423c8d594ef42bf6c598c1b192fbe2023062
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-29 07:55:01 +0530

    Add files via upload

---
+++ chapter4.py
@@ -0,0 +1,13 @@
+import sys
+from fractions import Fractions
+
+
+Ideal_temp = 95.5
+current_temp= 95.554556456456
+
+print(f"Ideal temperature: {Ideal_temp}°C  ")
+print(f"Current temperature: {current_temp}°C  ")
+print(f"difference in temperature: {Ideal_temp - current_temp}°C    ")
+print(sys.float_info)
+
+
