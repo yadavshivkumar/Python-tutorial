09fecc3d594b7b8a7498448e823089cb46b6bc2c
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-01 10:55:10 +0530

    Add files via upload

---
+++ chapter7.py
@@ -0,0 +1,10 @@
+snacks = input("Enter your preferred Snacks").lower
+
+print(f"user said : {snacks}")
+
+if snacks == "samosa" or snacks == "cookies":
+    print(f"Great Choice We will serve you {snacks} ")
+
+else :
+    print("sorry we only serve samosa and cookies")
+    
