7bf31657dc0e4c7ff117b53b657e59df92b917d2
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-02 17:07:42 +0530

    Add files via upload

---
+++ chapter8.py
@@ -0,0 +1,14 @@
+cup = input("Enter your preferred Cup size()small/medium/large): ").lower
+
+
+if cup =="small":
+    print("price is 10 rupees")
+elif cup == "medium":
+    print("price is 15 rupees")
+elif cup == "large":
+    print("price is 20 rupees")
+else:
+    print("sorry, unknown cup size")  
+
+
+    
