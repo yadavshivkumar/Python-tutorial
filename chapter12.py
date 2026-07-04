1ed797f229e9845780d404a620ec4106530b3b53
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-04 06:54:05 +0530

    Add files via upload

---
+++ chapter12.py
@@ -0,0 +1,16 @@
+# loops are statements that allow code to be executed repeatedly
+
+#for loop are used to iterate over a sequence (like a list, tuple, dictionary, set, or string) and execute a block of code for each item in the sequence.   
+
+# while loops are used to execute a block of code as long as a specified condition is true. 
+
+for i in range(5):
+    print(i)  # This will print numbers from 0 to 4 
+    
+    
+    
+count = 0
+while count < 5:
+    print(count)  # This will print numbers from 0 to 4
+    count += 1  # Increment count by 1  
+    
