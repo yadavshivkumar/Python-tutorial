6c7f7f743613b0d953c353a746e4dcdf6f5b0d7e
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-06 13:32:41 +0530

    Add files via upload

---
+++ chapter17.py
@@ -0,0 +1,9 @@
+temperature = 40 
+
+while temperature < 100:
+    print(f"Current temperature: {temperature}°C")
+    temperature += 15
+
+
+print(f"Tea is ready to serve at {temperature}°C!   ")
+
