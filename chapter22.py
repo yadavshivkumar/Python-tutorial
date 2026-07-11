214158880dbfa3c3f81ec7a34146538b2763355f
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-11 12:05:59 +0530

    Add files via upload

---
+++ chapter22.py
@@ -0,0 +1,9 @@
+def print_order (name, chai_type):
+    print(f"{name} ordered a {chai_type} chai.")
+
+
+
+print_order("Alice", "Green")
+print_order("Shiv", "Masala")
+print_order("Alice", "Ginger")
+print_order("Alice", "Tulsi")
