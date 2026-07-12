692a512c5a537b3c3a960d8b5ec974b31921cba6
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-12 06:06:45 +0530

    Add files via upload

---
+++ chapter23.py
@@ -0,0 +1,16 @@
+def fetch_sales():
+    print("Fetching sales data")
+
+def filter_valid_sales():
+    print("Filtering valid sales data")
+
+def summarize_data():
+    print("Summarizing sales data") 
+    
+def generate_report():
+    fetch_sales()
+    filter_valid_sales()
+    summarize_data()
+    print(" sales report ready ")
+    
+generate_report()
