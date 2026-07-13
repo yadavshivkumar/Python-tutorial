b48c44a3c1c3ba163176c6e851d271cb17c072e7
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-13 06:35:01 +0530

    Add files via upload

---
+++ chapter24.py
@@ -0,0 +1,16 @@
+def get_input():
+    print("Getting user input...")
+    
+def validate_input():
+    print("Validating user input...")
+    
+def save_to_db():
+    print("Saving data to the database...")
+    
+def register_user():
+    get_input()
+    validate_input()
+    save_to_db()
+    print("User registered successfully!")  
+    
+register_user()
