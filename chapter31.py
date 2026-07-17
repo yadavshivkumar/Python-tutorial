6a9943869fdaa13edee890ac5becff13eb0bfd94
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-17 06:48:30 +0530

    Add files via upload

---
+++ chapter31.py
@@ -0,0 +1,11 @@
+menu = [
+    "Masala Chai ",
+    "Ginger Chai ",
+    "Iced Peach Tea ",
+    "Cardamom Chai ",
+    "Iced Lemon Tea "
+]
+
+iced_tea = [tea for tea in menu  if "Iced" in tea]
+
+print(iced_tea) 
